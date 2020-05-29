d48a38d462581ab569ccaa8e6cb5ce0a14c72482
Zhangir Siranov <djangomlgpro@gmail.com>
2020-05-30 05:30:46 +0600

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,111 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primarySwatch: Colors.teal,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: AuthCheck(),
+    );
+  }
+}
+
+
+class AuthCheck extends StatefulWidget {
+  @override
+  _AuthCheckState createState() => _AuthCheckState();
+}
+
+class _AuthCheckState extends State<AuthCheck> {
+
+  var reged;
+  checkAuth()async{
+   final user =  await FirebaseAuth.instance.currentUser();
+   if(user!=null){
+     await getUserDoc();
+     reged = true;
+   }else{
+     reged = false;
+   }
+   Future.delayed(Duration(milliseconds: 200), (){
+     setState((){});
+   });
+  }
+
+
+  @override
+  void initState() {
+    checkAuth();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSwitcher(
+      duration: Duration(milliseconds: 500),
+          child: reged!=null?(reged?HomePage():Authorization()):Scaffold(
+        backgroundColor: Colors.teal,
+            body: Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Icon(Icons.assignment, color: Colors.white, size: 50),
+                  Padding(
+                    padding: EdgeInsets.only(top: 10),
+                    child: Text('Exotic Matter', style: TextStyle(
+                      color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold, 
+                    )),
+                  ),
+                ],
+              ),
+            ),
+      ),
+    );
+  }
+}
+
+getUserDoc()async{
+  print('get the user data');
+}
+
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
+
+
+class Authorization extends StatefulWidget {
+  @override
+  _AuthorizationState createState() => _AuthorizationState();
+}
+
+class _AuthorizationState extends State<Authorization> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
